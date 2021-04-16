7505e697db78bf53bf08d1fb93a18e6ea7552e67
jogiter <jogiter.g@gmail.com>
2021-04-16 10:39:27 +0800

    feat: fix VueLoaderPlugin import to support vue-loader@16.x

--- app/vue/src/server/framework-preset-vue.ts
+++ app/vue/src/server/framework-preset-vue.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-param-reassign */
-import VueLoaderPlugin from 'vue-loader/lib/plugin';
+import { VueLoaderPlugin } from 'vue-loader';
 import type { Configuration } from 'webpack';
 
 export function webpack(config: Configuration) {
