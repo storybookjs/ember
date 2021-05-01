43ab07465ae723e42c65f99a0e1642fb454aca97
Michael Shilman <michael@lab80.co>
2021-05-01 21:29:21 +0800

    Fix DeepScan

--- examples/official-storybook/stories/demo/typed-button.stories.tsx
+++ examples/official-storybook/stories/demo/typed-button.stories.tsx
@@ -1,4 +1,4 @@
-import React, { ComponentProps } from 'react';
+import React from 'react';
 import { Meta, Story, ComponentStory } from '@storybook/react';
 import TsButton from '../../components/TsButton';
 
