0a40da08385b02a607bbb1c9a5b1aa2db33401b6
Gaëtan Maisse <gaetanmaisse@gmail.com>
2021-05-04 22:16:48 +0200

    refactor(docs): extract prepareForInline and fix to TS errors

--- addons/docs/src/frameworks/lit/config.ts
+++ addons/docs/src/frameworks/lit/config.ts
@@ -1,28 +1,8 @@
-/* global window */
-import React from 'react';
-import { render, TemplateResult } from 'lit';
+import { prepareForInline } from './prepareForInline';
 
 export const parameters = {
   docs: {
     inlineStories: true,
-    prepareForInline: (storyFn: () => TemplateResult) => {
-      class Story extends React.Component {
-        private readonly wrapperRef: React.RefObject<HTMLElement>;
-
-        constructor(props: unknown) {
-          super(props);
-          this.wrapperRef = React.createRef();
-        }
-
-        componentDidMount() {
-          render(storyFn(), this.wrapperRef.current);
-        }
-
-        render() {
-          return React.createElement('div', { ref: this.wrapperRef });
-        }
-      }
-      return React.createElement(Story);
-    },
+    prepareForInline,
   },
 };
