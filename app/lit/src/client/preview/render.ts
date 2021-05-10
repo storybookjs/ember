fa82ecaa008013add98b7fdc7dca63354483b87b
Gaëtan Maisse <gaetanmaisse@gmail.com>
2021-05-10 08:03:51 +0200

    feat(lit): fix Deepscan warning and strengthen TypeScript compilation options

--- app/lit/src/client/preview/render.ts
+++ app/lit/src/client/preview/render.ts
@@ -1,20 +1,12 @@
-import { document, Node } from 'global';
+import { document } from 'global';
 import dedent from 'ts-dedent';
 import { render } from 'lit';
 import { isTemplateResult } from 'lit/directive-helpers.js';
-// import { simulatePageLoad, simulateDOMContentLoaded } from '@storybook/client-api';
 import { RenderContext } from './types';
 
 const rootElement = document.getElementById('root');
 
-export default function renderMain({
-  storyFn,
-  kind,
-  name,
-  showMain,
-  showError,
-  forceRender,
-}: RenderContext) {
+export default function renderMain({ storyFn, kind, name, showMain, showError }: RenderContext) {
   const element = storyFn();
 
   showMain();
