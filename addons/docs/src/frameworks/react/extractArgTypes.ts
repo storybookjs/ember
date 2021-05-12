25e9208e8a882ec766077c3f250f5af4cdff585b
Tom Coleman <tom@thesnail.org>
2021-05-12 12:54:11 +1000

    Remove all defaultValue eval-ing in addon-docs

--- addons/docs/src/frameworks/react/extractArgTypes.ts
+++ addons/docs/src/frameworks/react/extractArgTypes.ts
@@ -7,26 +7,18 @@ export const extractArgTypes: ArgTypesExtractor = (component) => {
     const { rows } = extractProps(component);
     if (rows) {
       return rows.reduce((acc: ArgTypes, row: PropDef) => {
-        const { name, type, sbType, defaultValue: defaultSummary, jsDocTags, required } = row;
+        const {
+          name,
+          type,
+          sbType,
+          defaultValue: defaultSummary,
+          jsDocTags,
+          required,
+          ...rest
+        } = row;
 
-        let defaultValue;
-        if (component.defaultProps) {
-          defaultValue = component.defaultProps[name];
-        } else {
-          const defaultValueString =
-            defaultSummary && (defaultSummary.detail || defaultSummary.summary);
-          try {
-            if (defaultValueString) {
-              // eslint-disable-next-line no-new-func
-              defaultValue = Function(`"use strict";return (${defaultValueString})`)();
-            }
-            // eslint-disable-next-line no-empty
-          } catch {}
-        }
-
-        acc[row.name] = {
-          ...row,
-          defaultValue,
+        acc[name] = {
+          ...rest,
           type: { required, ...sbType },
           table: {
             type,
