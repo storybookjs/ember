90213436c7d056b23e895628c50207d4f2bb65d5
Tom Coleman <tom@thesnail.org>
2021-05-14 14:57:14 +1000

    Remove unused code

--- addons/docs/src/frameworks/vue/extractArgTypes.ts
+++ addons/docs/src/frameworks/vue/extractArgTypes.ts
@@ -1,11 +1,8 @@
 import { ArgTypes } from '@storybook/api';
 import { ArgTypesExtractor, hasDocgen, extractComponentProps } from '../../lib/docgen';
 import { convert } from '../../lib/convert';
-import { trimQuotes } from '../../lib/convert/utils';
 
 const SECTIONS = ['props', 'events', 'slots'];
-
-const trim = (val: any) => (val && typeof val === 'string' ? trimQuotes(val) : val);
 
 export const extractArgTypes: ArgTypesExtractor = (component) => {
   if (!hasDocgen(component)) {
