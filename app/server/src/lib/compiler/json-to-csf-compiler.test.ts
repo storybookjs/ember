d1be58decffe61dfcdf983491dbfce034108eb4a
Titouan Mathis <titouan@studiometa.fr>
2021-05-31 15:31:50 +0200

    Group tests by file types

--- app/server/src/lib/compiler/json-to-csf-compiler.test.ts
+++ app/server/src/lib/compiler/json-to-csf-compiler.test.ts
@@ -4,24 +4,26 @@ import fs from 'fs-extra';
 import YAML from 'yaml';
 import { compileCsfModule } from '.';
 
-const inputRegExp = /\.(json|ya?ml)$/;
-
 async function generate(filePath: string) {
   const content = await fs.readFile(filePath, 'utf8');
   const parsed = filePath.endsWith('.json') ? JSON.parse(content) : YAML.parse(content);
   return compileCsfModule(parsed);
 }
 
-describe('json-to-csf-compiler', () => {
-  const transformFixturesDir = path.join(__dirname, '__testfixtures__');
-  fs.readdirSync(transformFixturesDir)
-    .filter((fileName: string) => inputRegExp.test(fileName))
-    .forEach((fixtureFile: string) => {
-      // eslint-disable-next-line jest/valid-title
-      it(fixtureFile, async () => {
-        const inputPath = path.join(transformFixturesDir, fixtureFile);
-        const code = await generate(inputPath);
-        expect(code).toMatchSpecificSnapshot(inputPath.replace(inputRegExp, '.snapshot'));
+['json', 'ya?ml'].forEach((fileType) => {
+  const inputRegExp = new RegExp(`.${fileType}$`);
+
+  describe(`${fileType}-to-csf-compiler`, () => {
+    const transformFixturesDir = path.join(__dirname, '__testfixtures__');
+    fs.readdirSync(transformFixturesDir)
+      .filter((fileName: string) => inputRegExp.test(fileName))
+      .forEach((fixtureFile: string) => {
+        // eslint-disable-next-line jest/valid-title
+        it(fixtureFile, async () => {
+          const inputPath = path.join(transformFixturesDir, fixtureFile);
+          const code = await generate(inputPath);
+          expect(code).toMatchSpecificSnapshot(inputPath.replace(inputRegExp, '.snapshot'));
+        });
       });
-    });
+  });
 });
