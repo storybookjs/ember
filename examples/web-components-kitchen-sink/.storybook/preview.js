53d2b4a210f3514f7a19b4be39393a95de2804fd
Gaëtan Maisse <gaetanmaisse@gmail.com>
2021-06-19 18:03:41 +0200

    example(web-components): setup custom elements manifest v1

--- examples/web-components-kitchen-sink/.storybook/preview.js
+++ examples/web-components-kitchen-sink/.storybook/preview.js
@@ -1,24 +1,11 @@
-import {
-  // setCustomElementsManifest,
-  // ☝️☝️☝️☝️☝️☝️
-  // @TODO: cant import this function for some reason after running `yarn bootstrap --core`
-  setCustomElements,
-} from '@storybook/web-components';
+import { setCustomElementsManifest } from '@storybook/web-components';
 
-import customElements from '../custom-elements.json';
-import customElementsV1 from '../custom-elements-v1.json';
+import customElementsManifest from '../custom-elements.json';
 
-/**
- * @TODO: Need to remove this, but had to use it to test, because I cant seem to import the function
- */
-// eslint-disable-next-line
-function setCustomElementsManifest(customElements) {
-  // eslint-disable-next-line
-  // @ts-ignore
-  window.__STORYBOOK_CUSTOM_ELEMENTS_MANIFEST__ = customElements;
-}
+setCustomElementsManifest(customElementsManifest);
 
-setCustomElementsManifest(customElementsV1);
+// TODO: Remove support of 0.x CustomElementManifest format in 7.0
+// import customElements from '../custom-elements-experimental.json';
 // setCustomElements(customElements);
 
 export const parameters = {
