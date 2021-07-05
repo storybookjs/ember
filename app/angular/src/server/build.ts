4e1bd0decd8afe545ace9202073168ec21dade17
Juho Vepsäläinen <bebraw@gmail.com>
2021-07-05 14:27:02 +0300

    fix(angular): Quit process after finishing a static build
    I don't understand why `process.exit` is needed for this particular case
    but it seems to fix the issue.
    
    In order to be more safe, I also added a `try`/`catch` and in my own
    testing that caught a ngcc locking issue I would have missed otherwise.
    
    Closes #15227.

--- app/angular/src/server/build.ts
+++ app/angular/src/server/build.ts
@@ -1,4 +1,16 @@
 import { buildStatic } from '@storybook/core/server';
+import { logger } from '@storybook/node-logger';
 import options from './options';
 
-buildStatic(options);
+async function build() {
+  try {
+    await buildStatic(options);
+
+    // #15227
+    process.exit(0);
+  } catch (error) {
+    logger.error(error);
+  }
+}
+
+build();
