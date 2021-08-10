b41ba6fb60918ff67a7605275bb672e3f7c15e70
Michael Shilman <michael@lab80.co>
2021-08-10 19:15:46 +0800

    Core: MDX support for built-in stories.json

--- lib/csf-tools/src/index.ts
+++ lib/csf-tools/src/index.ts
@@ -1,1 +1,15 @@
+import fs from 'fs-extra';
+import mdx from '@mdx-js/mdx';
+
+import { loadCsf } from './CsfFile';
+import { createCompiler } from './mdx';
+
+export const readCsfOrMdx = async (fileName: string) => {
+  let code = (await fs.readFile(fileName, 'utf-8')).toString();
+  if (fileName.endsWith('.mdx')) {
+    code = await mdx(code, { compilers: [createCompiler({})] });
+  }
+  return loadCsf(code);
+};
+
 export * from './CsfFile';
