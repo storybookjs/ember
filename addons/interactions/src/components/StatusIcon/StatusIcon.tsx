2515dc0bd88d42087c71a87ee4921162811e2491
Deen Denno <darleendenno@gmail.com>
2021-08-18 13:04:31 -0500

    styles: update status icon to use theme

--- addons/interactions/src/components/StatusIcon/StatusIcon.tsx
+++ addons/interactions/src/components/StatusIcon/StatusIcon.tsx
@@ -2,10 +2,17 @@ import React from 'react';
 import { Icons, IconsProps } from '@storybook/components';
 import { styled } from '@storybook/theming';
 import { TestState, TestingStates } from '../../Panel';
+import { theme } from '../../theme';
 
 export interface StatusIconProps extends IconsProps {
   status: TestState;
 }
+
+const {
+  colors: {
+    pure: { green, red, gray },
+  },
+} = theme;
 
 const StyledStatusIcon = styled(Icons)(({ status }: StatusIconProps) => ({
   flexShrink: 0,
@@ -13,8 +20,7 @@ const StyledStatusIcon = styled(Icons)(({ status }: StatusIconProps) => ({
   height: 12,
   padding: 1,
   marginRight: 5,
-  color:
-    status === TestingStates.PENDING ? 'gray' : status === TestingStates.DONE ? 'green' : 'red',
+  color: status === TestingStates.PENDING ? gray[500] : status === TestingStates.DONE ? green : red,
 }));
 
 /**
