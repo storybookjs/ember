1c57d01094da4fab83d4361070e8a6e973f488a6
Clément DUNGLER <clementdungler@gmail.com>
2021-08-22 18:16:18 +0200

    chore: add addon-ie11 to CRA

--- examples/cra-ts-kitchen-sink/.storybook/main.ts
+++ examples/cra-ts-kitchen-sink/.storybook/main.ts
@@ -7,6 +7,7 @@ module.exports = {
   logLevel: 'debug',
   addons: [
     '@storybook/preset-create-react-app',
+    '@storybook/addon-ie11',
     '@storybook/addon-docs',
     '@storybook/addon-actions',
     '@storybook/addon-links',
