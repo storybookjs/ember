cfcde69fcaf79a3084c311bd2f70812dc20dbaea
Deen Denno <darleendenno@gmail.com>
2021-08-23 18:13:41 -0500

    refactor: badge text and bg color logic into object lookups

--- addons/interactions/src/components/StatusBadge/StatusBadge.tsx
+++ addons/interactions/src/components/StatusBadge/StatusBadge.tsx
@@ -12,22 +12,32 @@ const {
   },
 } = theme;
 
-const StyledBadge = styled.div(({ status }: StatusBadgeProps) => ({
-  padding: '4px 8px',
-  borderRadius: '4px',
-  background: status === TestingStates.DONE ? green : status === TestingStates.ERROR ? red : ochre,
-  color: 'white',
-  fontFamily: typography.fonts.base,
-  textTransform: 'uppercase',
-  fontSize: typography.size.s1,
-  letterSpacing: 3,
-  fontWeight: 500,
-  width: 65,
-  textAlign: 'center',
-}));
+const StyledBadge = styled.div(({ status }: StatusBadgeProps) => {
+  const backgroundColor = {
+    [TestingStates.DONE]: green,
+    [TestingStates.ERROR]: red,
+    [TestingStates.PENDING]: ochre,
+  }[status];
+  return {
+    padding: '4px 8px',
+    borderRadius: '4px',
+    backgroundColor: backgroundColor,
+    color: 'white',
+    fontFamily: typography.fonts.base,
+    textTransform: 'uppercase',
+    fontSize: typography.size.s1,
+    letterSpacing: 3,
+    fontWeight: 500,
+    width: 65,
+    textAlign: 'center',
+  };
+});
 
 export const StatusBadge: React.FC<StatusBadgeProps> = ({ status }) => {
-  const badgeText =
-    status === TestingStates.ERROR ? 'Fail' : status === TestingStates.DONE ? 'Pass' : 'Runs';
+  const badgeText = {
+    [TestingStates.DONE]: 'Pass',
+    [TestingStates.ERROR]: 'Fail',
+    [TestingStates.PENDING]: 'Runs',
+  }[status];
   return <StyledBadge status={status}>{badgeText}</StyledBadge>;
 };
