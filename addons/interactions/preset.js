fc1f599607a45d8ee9cbb45f47b798190fb56859
Gert Hengeveld <info@ghengeveld.nl>
2021-08-27 23:06:07 +0200

    Add storySource

--- addons/interactions/preset.js
+++ addons/interactions/preset.js
@@ -6,7 +6,30 @@ function managerEntries(entry = []) {
   return [...entry, require.resolve('./dist/esm/preset/manager')];
 }
 
+function webpack(config = {}, options = {}) {
+  const sourceLoader = {
+    loader: require.resolve('@storybook/source-loader'),
+    options: options.loaderOptions,
+  }
+  return {
+    ...config,
+    module: {
+      ...config.module,
+      rules: [
+        ...(config.module?.rules || []),
+        {
+          test: [/\.stories\.(jsx?$|tsx?$)/],
+          ...options.rule,
+          enforce: 'pre',
+          use: [sourceLoader],
+        },
+      ],
+    },
+  };
+}
+
 module.exports = {
-  managerEntries,
   config,
+  managerEntries,
+  webpack,
 };
