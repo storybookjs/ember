4c09e018e1ab133922a5b16ee4c6de6e7a6ad433
Norbert de Langen <ndelangen@me.com>
2021-08-27 14:38:24 +0200

    fix non-spreadable properties on now non-existing default babel config

--- lib/manager-webpack5/src/presets/babel-loader-manager.ts
+++ lib/manager-webpack5/src/presets/babel-loader-manager.ts
@@ -1,27 +1,29 @@
 import { RuleSetRule } from 'webpack';
 import { getProjectRoot, getStorybookBabelConfig } from '@storybook/core-common';
 
-const { plugins, presets } = getStorybookBabelConfig();
+export const babelLoader: () => RuleSetRule = () => {
+  const { plugins, presets } = getStorybookBabelConfig();
 
-export const babelLoader: () => RuleSetRule = () => ({
-  test: /\.(mjs|tsx?|jsx?)$/,
-  use: [
-    {
-      loader: require.resolve('babel-loader'),
-      options: {
-        sourceType: 'unambiguous',
-        presets: [...presets, require.resolve('@babel/preset-react')],
-        plugins: [
-          ...plugins,
-          // Should only be done on manager. Template literals are not meant to be
-          // transformed for frameworks like ember
-          require.resolve('@babel/plugin-transform-template-literals'),
-        ],
-        babelrc: false,
-        configFile: false,
+  return {
+    test: /\.(mjs|tsx?|jsx?)$/,
+    use: [
+      {
+        loader: require.resolve('babel-loader'),
+        options: {
+          sourceType: 'unambiguous',
+          presets: [...presets, require.resolve('@babel/preset-react')],
+          plugins: [
+            ...plugins,
+            // Should only be done on manager. Template literals are not meant to be
+            // transformed for frameworks like ember
+            require.resolve('@babel/plugin-transform-template-literals'),
+          ],
+          babelrc: false,
+          configFile: false,
+        },
       },
-    },
-  ],
-  include: [getProjectRoot()],
-  exclude: [/node_modules/, /dist/],
-});
+    ],
+    include: [getProjectRoot()],
+    exclude: [/node_modules/, /dist/],
+  };
+};
