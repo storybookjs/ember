7d3b6b88d84e3d823bb6a7f9e161cea8404a4752
Tom Coleman <tom@thesnail.org>
2021-08-30 10:46:12 +1000

    Added a test for the combined case

--- lib/core-client/src/preview/start.test.ts
+++ lib/core-client/src/preview/start.test.ts
@@ -2,14 +2,11 @@ import global from 'global';
 import { EventEmitter } from 'events';
 import Events from '@storybook/core-events';
 
+import { waitForRender, mockChannel } from '@storybook/web-preview/dist/cjs/WebPreview.mockdata';
+
 import { start } from './start';
-import {
-  emitter,
-  mockChannel,
-  waitForEvents,
-  waitForRender,
-  waitForQuiescence,
-} from '../../../web-preview/src/WebPreview.mockdata';
+
+jest.mock('@storybook/web-preview/dist/cjs/WebView');
 
 const { history, document } = global;
 jest.mock('global', () => ({
@@ -21,18 +18,6 @@ jest.mock('global', () => ({
       pathname: 'pathname',
       search: '?id=*',
     },
-    // TODO -- move start into web-preview so we can mock web-view?
-    getElementById: jest.fn().mockReturnValue({
-      setAttribute: jest.fn(),
-      removeAttribute: jest.fn(),
-    }),
-    body: {
-      classList: {
-        remove: jest.fn(),
-        add: jest.fn(),
-      },
-    },
-    documentElement: {},
   },
 }));
 
@@ -47,18 +32,21 @@ describe('start', () => {
 
       configure('test', () => {
         clientApi
-          .storiesOf('Component A', { id: 'file1' })
+          .storiesOf('Component A', { id: 'file1' } as NodeModule)
           .add('Story One', jest.fn())
           .add('Story Two', jest.fn());
 
-        clientApi.storiesOf('Component B', { id: 'file2' }).add('Story Three', jest.fn());
+        clientApi
+          .storiesOf('Component B', { id: 'file2' } as NodeModule)
+          .add('Story Three', jest.fn());
       });
 
       await waitForRender();
 
       expect(mockChannel.emit).toHaveBeenCalledWith(Events.SET_STORIES, expect.any(Object));
-      expect(mockChannel.emit.mock.calls.find((c) => c[0] === Events.SET_STORIES)[1])
-        .toMatchInlineSnapshot(`
+      expect(
+        mockChannel.emit.mock.calls.find((call: [string, any]) => call[0] === Events.SET_STORIES)[1]
+      ).toMatchInlineSnapshot(`
         Object {
           "globalParameters": Object {},
           "globals": Object {},
@@ -105,7 +93,7 @@ describe('start', () => {
         expect.objectContaining({
           id: 'component-a--story-one',
         }),
-        expect.any(Object)
+        undefined
       );
     });
 
@@ -115,7 +103,7 @@ describe('start', () => {
       const { configure, clientApi } = start(render);
 
       configure('test', () => {
-        clientApi.storiesOf('Component A', { id: 'file1' }).add('default', jest.fn());
+        clientApi.storiesOf('Component A', { id: 'file1' } as NodeModule).add('default', jest.fn());
       });
 
       await waitForRender();
@@ -124,9 +112,9 @@ describe('start', () => {
     });
   });
 
-  const componentThreeExports = {
+  const componentCExports = {
     default: {
-      title: 'Component Three',
+      title: 'Component C',
     },
     StoryOne: jest.fn(),
     StoryTwo: jest.fn(),
@@ -137,32 +125,32 @@ describe('start', () => {
       const render = jest.fn();
 
       const { configure } = start(render);
-      configure('test', () => [componentThreeExports]);
+      configure('test', () => [componentCExports]);
 
       await waitForRender();
-      console.dir(mockChannel.emit.mock.calls, { depth: 4 });
 
       expect(mockChannel.emit).toHaveBeenCalledWith(Events.SET_STORIES, expect.any(Object));
-      expect(mockChannel.emit.mock.calls.find((c) => c[0] === Events.SET_STORIES)[1])
-        .toMatchInlineSnapshot(`
+      expect(
+        mockChannel.emit.mock.calls.find((call: [string, any]) => call[0] === Events.SET_STORIES)[1]
+      ).toMatchInlineSnapshot(`
         Object {
           "globalParameters": Object {},
           "globals": Object {},
           "kindParameters": Object {
-            "Component Three": Object {},
+            "Component C": Object {},
           },
           "stories": Object {
-            "component-three--story-one": Object {
-              "id": "component-three--story-one",
-              "kind": "Component Three",
+            "component-c--story-one": Object {
+              "id": "component-c--story-one",
+              "kind": "Component C",
               "name": "Story One",
               "parameters": Object {
                 "fileName": "exports-map-0",
               },
             },
-            "component-three--story-two": Object {
-              "id": "component-three--story-two",
-              "kind": "Component Three",
+            "component-c--story-two": Object {
+              "id": "component-c--story-two",
+              "kind": "Component C",
               "name": "Story Two",
               "parameters": Object {
                 "fileName": "exports-map-0",
@@ -175,17 +163,107 @@ describe('start', () => {
 
       expect(mockChannel.emit).toHaveBeenCalledWith(
         Events.STORY_RENDERED,
-        'component-three--story-one'
+        'component-c--story-one'
       );
 
       expect(render).toHaveBeenCalledWith(
         expect.objectContaining({
-          id: 'component-three--story-one',
+          id: 'component-c--story-one',
         }),
-        expect.any(Object)
+        undefined
       );
     });
   });
 
-  describe('when configure is called with a combination', () => {});
+  describe('when configure is called with a combination', () => {
+    it('loads and renders the first story correctly', async () => {
+      const render = jest.fn();
+
+      const { configure, clientApi } = start(render);
+      configure('test', () => {
+        clientApi
+          .storiesOf('Component A', { id: 'file1' } as NodeModule)
+          .add('Story One', jest.fn())
+          .add('Story Two', jest.fn());
+
+        clientApi
+          .storiesOf('Component B', { id: 'file2' } as NodeModule)
+          .add('Story Three', jest.fn());
+
+        return [componentCExports];
+      });
+
+      await waitForRender();
+
+      expect(mockChannel.emit).toHaveBeenCalledWith(Events.SET_STORIES, expect.any(Object));
+      expect(
+        mockChannel.emit.mock.calls.find((call: [string, any]) => call[0] === Events.SET_STORIES)[1]
+      ).toMatchInlineSnapshot(`
+        Object {
+          "globalParameters": Object {},
+          "globals": Object {},
+          "kindParameters": Object {
+            "Component A": Object {},
+            "Component B": Object {},
+            "Component C": Object {},
+          },
+          "stories": Object {
+            "component-a--story-one": Object {
+              "id": "component-a--story-one",
+              "kind": "Component A",
+              "name": "Story One",
+              "parameters": Object {
+                "fileName": "file1",
+              },
+            },
+            "component-a--story-two": Object {
+              "id": "component-a--story-two",
+              "kind": "Component A",
+              "name": "Story Two",
+              "parameters": Object {
+                "fileName": "file1",
+              },
+            },
+            "component-b--story-three": Object {
+              "id": "component-b--story-three",
+              "kind": "Component B",
+              "name": "Story Three",
+              "parameters": Object {
+                "fileName": "file2",
+              },
+            },
+            "component-c--story-one": Object {
+              "id": "component-c--story-one",
+              "kind": "Component C",
+              "name": "Story One",
+              "parameters": Object {
+                "fileName": "exports-map-0",
+              },
+            },
+            "component-c--story-two": Object {
+              "id": "component-c--story-two",
+              "kind": "Component C",
+              "name": "Story Two",
+              "parameters": Object {
+                "fileName": "exports-map-0",
+              },
+            },
+          },
+          "v": 3,
+        }
+      `);
+
+      expect(mockChannel.emit).toHaveBeenCalledWith(
+        Events.STORY_RENDERED,
+        'component-a--story-one'
+      );
+
+      expect(render).toHaveBeenCalledWith(
+        expect.objectContaining({
+          id: 'component-a--story-one',
+        }),
+        undefined
+      );
+    });
+  });
 });
