5852b041e663cd9f3b6e23ec49751f0c8f0082a5
Gert Hengeveld <info@ghengeveld.nl>
2021-09-01 09:03:44 +0200

    Missing type

--- addons/interactions/src/time.ts
+++ addons/interactions/src/time.ts
@@ -1,7 +1,7 @@
 import { instrument } from './instrument';
 
 export const { sleep } = instrument(
-  { sleep: (ms) => new Promise((r) => setTimeout(r, ms)) },
+  { sleep: (ms: number) => new Promise((r) => setTimeout(r, ms)) },
   { intercept: true }
 );
 
