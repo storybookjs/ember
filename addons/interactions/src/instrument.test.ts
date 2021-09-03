0c23516344a922965d69bab85a81ace0a0fff51c
Gert Hengeveld <info@ghengeveld.nl>
2021-09-03 13:32:02 +0200

    Add tests and fix chaining on plain object return values

---
+++ addons/interactions/src/instrument.test.ts
@@ -0,0 +1,309 @@
+import { addons, mockChannel } from '@storybook/addons';
+import global from 'global';
+import { EVENTS } from './constants';
+import { instrument } from './instrument';
+
+const callSpy = jest.fn();
+addons.setChannel(mockChannel());
+addons.getChannel().on(EVENTS.CALL, callSpy);
+
+class Element {
+  constructor(props: any) {
+    Object.assign(this, props);
+  }
+}
+global.window = {
+  Element,
+  location: { reload: jest.fn() },
+  __STORYBOOK_ADDON_TEST_PREVIEW__: {},
+  parent: { __STORYBOOK_ADDON_TEST_MANAGER__: {} },
+};
+
+beforeEach(() => {
+  callSpy.mockReset();
+  addons.getChannel().emit(EVENTS.RESET);
+  global.window.__STORYBOOK_ADDON_TEST_PREVIEW__.n = 0;
+  global.window.__STORYBOOK_ADDON_TEST_PREVIEW__.next = {};
+  global.window.__STORYBOOK_ADDON_TEST_PREVIEW__.callRefsByResult = new Map();
+  global.window.parent.__STORYBOOK_ADDON_TEST_MANAGER__.isDebugging = false;
+  global.window.parent.__STORYBOOK_ADDON_TEST_MANAGER__.chainedCallIds = new Set();
+  global.window.parent.__STORYBOOK_ADDON_TEST_MANAGER__.playUntil = undefined;
+});
+
+describe('instrument', () => {
+  it('patches object methods', () => {
+    const fn = () => {};
+    const result = instrument({ fn });
+    expect(result).toStrictEqual({ fn: expect.any(Function) });
+    expect(result.fn.name).toBe('fn');
+    expect(result.fn._original).toBe(fn);
+  });
+
+  it('patches nested methods', () => {
+    const fn1 = () => {};
+    const fn2 = () => {};
+    const result = instrument({ foo: { fn1, bar: { fn2 } } });
+    expect(result).toStrictEqual({
+      foo: {
+        fn1: expect.any(Function),
+        bar: { fn2: expect.any(Function) },
+      },
+    });
+    expect(result.foo.fn1._original).toBe(fn1);
+    expect(result.foo.bar.fn2._original).toBe(fn2);
+  });
+
+  it('does not patch already patched functions', () => {
+    const fn = () => {};
+    const result = instrument(instrument({ fn }));
+    expect(result.fn._original).toBe(fn);
+    expect(result.fn._original._original).not.toBeDefined();
+  });
+
+  it('does not traverse into arrays', () => {
+    const fn1 = () => {};
+    const fn2 = () => {};
+    const result = instrument({ arr: [fn1, { fn2 }] });
+    expect(result).toStrictEqual({ arr: [fn1, { fn2 }] });
+    expect(result.arr[0]._original).not.toBeDefined();
+    expect(result.arr[1].fn2._original).not.toBeDefined();
+  });
+
+  it('patches function properties on functions', () => {
+    const fn1 = () => {};
+    fn1.fn2 = () => {};
+    const result = instrument({ fn1 });
+    expect(result.fn1).toEqual(expect.any(Function));
+    expect(result.fn1.fn2).toEqual(expect.any(Function));
+    expect(result.fn1._original).toBe(fn1);
+    expect(result.fn1.fn2._original).toBe(fn1.fn2);
+  });
+
+  it('patched functions call the original function when invoked', () => {
+    const { fn } = instrument({ fn: jest.fn() });
+    const obj = {};
+    fn('foo', obj);
+    expect(fn._original).toHaveBeenCalledWith('foo', obj);
+  });
+
+  it('emits a "call" event every time a patched function is invoked', () => {
+    const { fn } = instrument({ fn: () => {} });
+    fn('foo', 'bar');
+    fn('baz');
+    expect(callSpy).toHaveBeenCalledWith(
+      expect.objectContaining({
+        id: '0-fn',
+        args: ['foo', 'bar'],
+      })
+    );
+    expect(callSpy).toHaveBeenCalledWith(
+      expect.objectContaining({
+        id: '1-fn',
+        args: ['baz'],
+      })
+    );
+  });
+
+  it('provides metadata about the call in the event', () => {
+    const { obj } = instrument({ obj: { fn: () => {} } });
+    obj.fn();
+    expect(callSpy).toHaveBeenCalledWith(
+      expect.objectContaining({
+        path: ['obj'],
+        method: 'fn',
+        interceptable: false,
+        state: 'done',
+      })
+    );
+  });
+
+  it('maps event args which originate from an earlier call to a call ref', () => {
+    const { fn1, fn2 } = instrument({
+      fn1: (arg: any) => arg,
+      fn2: () => {},
+    });
+    fn2(fn1({}));
+    expect(callSpy).toHaveBeenLastCalledWith(
+      expect.objectContaining({
+        method: 'fn2',
+        args: [{ __callId__: callSpy.mock.calls[0][0].id }],
+      })
+    );
+  });
+
+  it('does not map primitive event args which originate from an earlier call', () => {
+    const { fn1, fn2 } = instrument({
+      fn1: (arg: any) => arg,
+      fn2: () => {},
+    });
+    fn2(
+      fn1(undefined),
+      fn1(null),
+      fn1(true),
+      fn1('foo'),
+      fn1(1),
+      fn1(BigInt(1)),
+      fn1({}),
+      fn1([]),
+      fn1(() => {}),
+      fn1(Symbol())
+    );
+    expect(callSpy).toHaveBeenLastCalledWith(
+      expect.objectContaining({
+        method: 'fn2',
+        args: [
+          /* call 0 */ undefined,
+          /* call 1 */ null,
+          /* call 2 */ true,
+          /* call 3 */ 'foo',
+          /* call 4 */ 1,
+          /* call 5 */ BigInt(1),
+          { __callId__: callSpy.mock.calls[6][0].id },
+          { __callId__: callSpy.mock.calls[7][0].id },
+          { __callId__: callSpy.mock.calls[8][0].id },
+          { __callId__: callSpy.mock.calls[9][0].id },
+        ],
+      })
+    );
+  });
+
+  it('maps DOM Elements in event args to an element ref', () => {
+    const { fn } = instrument({ fn: () => {} });
+    fn(new Element({ prefix: '', localName: 'div', id: 'root', classList: [] }));
+    expect(callSpy).toHaveBeenLastCalledWith(
+      expect.objectContaining({
+        args: [{ __element__: { prefix: '', localName: 'div', id: 'root', classList: [] } }],
+      })
+    );
+  });
+
+  it('instruments the call result to support chaining', () => {
+    const { fn1 } = instrument({
+      fn1: () => ({
+        fn2: () => {},
+      }),
+    });
+    fn1().fn2();
+    expect(callSpy).toHaveBeenLastCalledWith(
+      expect.objectContaining({
+        method: 'fn2',
+        path: [{ __callId__: callSpy.mock.calls[0][0].id }],
+      })
+    );
+  });
+
+  it('emits a call event with exception metadata when the function throws', () => {
+    const { fn } = instrument({
+      fn: () => {
+        throw new Error('Boom!');
+      },
+    });
+    expect(fn).toThrow();
+    expect(callSpy).toHaveBeenCalledWith(
+      expect.objectContaining({
+        id: '0-fn',
+        exception: {
+          name: 'Error',
+          message: 'Boom!',
+          stack: expect.stringContaining('Error: Boom!'),
+        },
+      })
+    );
+  });
+
+  it('catches thrown errors and throws an ignoredException instead', () => {
+    const { fn } = instrument({
+      fn: () => {
+        throw new Error('Boom!');
+      },
+    });
+    expect(fn).not.toThrow('Boom!');
+    expect(fn).toThrow('ignoredException');
+  });
+
+  it("re-throws anything that isn't an error", () => {
+    const { fn } = instrument({
+      fn: () => {
+        throw 'Boom!';
+      },
+    });
+    expect(fn).toThrow('Boom!');
+    expect(callSpy).not.toHaveBeenCalled();
+  });
+
+  it('does not affect intercepted methods', () => {
+    const { fn } = instrument({ fn: jest.fn() }, { intercept: true });
+    fn('foo');
+    expect(fn._original).toHaveBeenCalledWith('foo');
+  });
+
+  it('reloads the page on the "reload" event', () => {
+    addons.getChannel().emit(EVENTS.RELOAD);
+    expect(global.window.location.reload).toHaveBeenCalled();
+  });
+
+  it('resets preview state on the "reset" event', () => {
+    global.window.__STORYBOOK_ADDON_TEST_PREVIEW__.n = 123;
+    global.window.__STORYBOOK_ADDON_TEST_PREVIEW__.next = { ref: () => {} };
+    global.window.__STORYBOOK_ADDON_TEST_PREVIEW__.callRefsByResult = new Map([[{}, 'ref']]);
+    addons.getChannel().emit(EVENTS.RESET);
+    expect(global.window.__STORYBOOK_ADDON_TEST_PREVIEW__).toStrictEqual({
+      n: 0,
+      next: {},
+      callRefsByResult: new Map(),
+    });
+  });
+
+  describe('while debugging', () => {
+    beforeEach(() => (global.window.parent.__STORYBOOK_ADDON_TEST_MANAGER__.isDebugging = true));
+
+    it('defers calls to intercepted functions', () => {
+      const { fn } = instrument({ fn: jest.fn() }, { intercept: true });
+      expect(fn()).toEqual(expect.any(Promise));
+      expect(fn._original).not.toHaveBeenCalled();
+    });
+
+    it('does not defer calls to non-intercepted functions', () => {
+      const { fn } = instrument({ fn: jest.fn(() => 'ok') });
+      expect(fn()).toBe('ok');
+      expect(fn._original).toHaveBeenCalled();
+    });
+
+    it('does not defer calls to intercepted functions that are chained upon', () => {
+      const { fn1 } = instrument({ fn1: jest.fn(() => ({ fn2: jest.fn() })) }, { intercept: true });
+      global.window.parent.__STORYBOOK_ADDON_TEST_MANAGER__.chainedCallIds.add('0-fn1');
+      const res1 = fn1();
+      expect(res1.fn2()).toEqual(expect.any(Promise));
+      expect(fn1._original).toHaveBeenCalled();
+      expect(res1.fn2._original).not.toHaveBeenCalled();
+    });
+
+    it('does not defer calls while playing until a certain call', () => {
+      const { fn } = instrument({ fn: jest.fn(() => 'ok') }, { intercept: true });
+      global.window.parent.__STORYBOOK_ADDON_TEST_MANAGER__.playUntil = '1-fn';
+      /* 0-fn */ expect(fn()).toBe('ok');
+      /* 1-fn */ expect(fn()).toBe('ok');
+      /* 2-fn */ expect(fn()).toEqual(expect.any(Promise));
+      /* 3-fn */ expect(fn()).toEqual(expect.any(Promise));
+    });
+
+    it('invokes the deferred function on the "next" event', (done) => {
+      const { fn } = instrument({ fn: jest.fn(() => 'bar') }, { intercept: true });
+      fn('foo').then((value: string) => {
+        expect(value).toBe('bar');
+        expect(fn._original).toHaveBeenCalledWith('foo');
+        done();
+      });
+      addons.getChannel().emit(EVENTS.NEXT);
+    });
+
+    it('resolves all pending promises on the "next" event', (done) => {
+      const { fn } = instrument({ fn: jest.fn(() => {}) }, { intercept: true });
+      Promise.all([fn(), fn()]).then(() => {
+        expect(fn._original).toHaveBeenCalledTimes(2);
+        done();
+      });
+      addons.getChannel().emit(EVENTS.NEXT);
+    });
+  });
+});
--- addons/interactions/src/instrument.ts
+++ addons/interactions/src/instrument.ts
@@ -1,11 +1,9 @@
-import { addons } from '@storybook/addons';
+import { addons, Channel } from '@storybook/addons';
 import { IGNORED_EXCEPTION } from '@storybook/core-events';
 import global from 'global';
 
 import { EVENTS } from './constants';
 import { Call, CallRef, CallState } from './types';
-
-const channel = addons.getChannel();
 
 export interface Options {
   intercept?: boolean;
@@ -13,30 +11,44 @@ export interface Options {
   path?: Array<string | CallRef>;
 }
 
-export interface PatchedFunction extends Function {
-  _original: Function;
+interface IframeState {
+  n: number;
+  next: Record<string, Function>;
+  callRefsByResult: Map<any, CallRef>;
 }
 
-global.window.__STORYBOOK_ADDON_TEST_PREVIEW__ = global.window.__STORYBOOK_ADDON_TEST_PREVIEW__ || {
-  n: 0,
-  next: undefined,
-  callRefsByResult: new Map(),
-};
+let channel: Channel;
+let iframeState: IframeState;
+let sharedState: any;
 
-const iframeState = global.window.__STORYBOOK_ADDON_TEST_PREVIEW__;
-const sharedState = global.window.parent.__STORYBOOK_ADDON_TEST_MANAGER__ || {
-  isDebugging: false,
-  chainedCallIds: new Set<Call['id']>(),
-  playUntil: undefined,
-};
+const init = () => {
+  if (!global.window.__STORYBOOK_ADDON_TEST_PREVIEW__) {
+    global.window.__STORYBOOK_ADDON_TEST_PREVIEW__ = {
+      n: 0,
+      next: {},
+      callRefsByResult: new Map(),
+    };
+  }
+  if (!global.window.parent.__STORYBOOK_ADDON_TEST_MANAGER__) {
+    global.window.parent.__STORYBOOK_ADDON_TEST_MANAGER__ = {
+      isDebugging: false,
+      chainedCallIds: new Set<Call['id']>(),
+      playUntil: undefined,
+    };
+  }
 
-channel.on(EVENTS.NEXT, () => iframeState.next && iframeState.next());
-channel.on(EVENTS.RELOAD, () => global.window.location.reload());
-channel.on(EVENTS.RESET, () => {
-  iframeState.n = 0;
-  iframeState.next = undefined;
-  iframeState.callRefsByResult.clear();
-});
+  channel = addons.getChannel();
+  iframeState = global.window.__STORYBOOK_ADDON_TEST_PREVIEW__;
+  sharedState = global.window.parent.__STORYBOOK_ADDON_TEST_MANAGER__;
+
+  channel.on(EVENTS.NEXT, () => Object.values(iframeState.next).forEach((resolve) => resolve()));
+  channel.on(EVENTS.RELOAD, () => global.window.location.reload());
+  channel.on(EVENTS.RESET, () => {
+    iframeState.n = 0;
+    iframeState.next = {};
+    iframeState.callRefsByResult.clear();
+  });
+};
 
 const isObject = (o: unknown) => Object.prototype.toString.call(o) === '[object Object]';
 const isModule = (o: unknown) => Object.prototype.toString.call(o) === '[object Module]';
@@ -58,14 +70,14 @@ function construct(obj: any) {
   }
 }
 
-function run(fn: Function, call: Call) {
+function invoke(fn: Function, call: Call) {
   // Map args that originate from a tracked function call to a call reference to enable nesting.
   // These values are often not fully serializable anyway (e.g. DOM elements).
   const mappedArgs = call.args.map((arg) => {
     if (iframeState.callRefsByResult.has(arg)) {
       return iframeState.callRefsByResult.get(arg);
     }
-    if (arg instanceof Element) {
+    if (arg instanceof global.window.Element) {
       const { prefix, localName, id, classList } = arg;
       return { __element__: { prefix, localName, id, classList } };
     }
@@ -80,9 +92,9 @@ function run(fn: Function, call: Call) {
   try {
     const result = fn(...call.args);
     channel.emit(EVENTS.CALL, { ...call, args: mappedArgs, state: CallState.DONE });
-    if (result && typeof result === 'object') {
+    if (result && ['object', 'function', 'symbol'].includes(typeof result)) {
       // Track the result so we can trace later uses of it back to the originating call.
-      // Primitive results (undefined, string, number, boolean) and null are ignored.
+      // Primitive results (undefined, null, boolean, string, number, BigInt) are ignored.
       iframeState.callRefsByResult.set(result, { __callId__: call.id });
     }
     return result;
@@ -101,15 +113,15 @@ function intercept(fn: Function, call: Call) {
   // For a "jump to step" action, continue playing until we hit a call by that ID.
   // For chained calls, we can only return a Promise for the last call in the chain.
   const isChainedUpon = sharedState.chainedCallIds.has(call.id);
-  if (sharedState.playUntil || isChainedUpon || !sharedState.isDebugging) {
+  if (!sharedState.isDebugging || isChainedUpon || sharedState.playUntil) {
     if (sharedState.playUntil === call.id) sharedState.playUntil = undefined;
-    return run(fn, call);
+    return invoke(fn, call);
   }
 
   // Instead of invoking the function, defer the function call until we continue playing.
-  return new Promise((resolve) => (iframeState.next = resolve))
-    .then(() => (iframeState.next = undefined))
-    .then(() => run(fn, call));
+  return new Promise((resolve) => (iframeState.next[call.id] = resolve))
+    .then(() => delete iframeState.next[call.id])
+    .then(() => invoke(fn, call));
 }
 
 // Monkey patch an object method to record calls.
@@ -118,14 +130,13 @@ function intercept(fn: Function, call: Call) {
 function track(method: string, fn: Function, args: any[], { path = [], ...options }: Options) {
   const id = `${iframeState.n++}-${method}`;
   const call: Call = { id, path, method, args, interceptable: !!options.intercept };
-  const result = (options.intercept ? intercept : run)(fn, call);
-  return instrument(result, { ...options, path: [{ __callId__: call.id }] });
+  const result = (options.intercept ? intercept : invoke)(fn, call);
+  return instrument(result, { ...options, mutate: true, path: [{ __callId__: call.id }] });
 }
 
-function patch(method: string, fn: Function, options: Options): PatchedFunction {
-  if ((fn as PatchedFunction)._original) return fn as PatchedFunction; // already patched
-  const patched: PatchedFunction = (...args: any[]) => track(method, fn, args, options);
-  patched._original = fn;
+function patch(method: string, fn: Function, options: Options) {
+  const patched = (...args: any[]) => track(method, fn, args, options);
+  Object.defineProperty(patched, 'name', { value: method, writable: false });
   return patched;
 }
 
@@ -134,20 +145,35 @@ function patch(method: string, fn: Function, options: Options): PatchedFunction 
 // depending on whether it should mutate.
 export function instrument(obj: unknown, options: Options = {}) {
   if (!isInstrumentable(obj)) return obj;
+  if (!channel) init();
+
   const { mutate = false, path = [] } = options;
   return Object.keys(obj).reduce(
     (acc, key) => {
       const value = (obj as Record<string, any>)[key];
-      if (typeof value === 'function') {
-        acc[key] = patch(key, value, options);
-        // Deal with functions that also act like an object
-        // TODO might be able to make functions instrumentable so we can omit this extra if statement
-        if (Object.keys(value).length > 0) {
-          Object.assign(acc[key], instrument({ ...value }, { ...options, path: path.concat(key) }));
-        }
-      } else {
+
+      // Nothing to patch, but might be instrumentable, so we recurse
+      if (typeof value !== 'function') {
         acc[key] = instrument(value, { ...options, path: path.concat(key) });
+        return acc;
       }
+
+      // Already patched, so we pass through unchanged
+      if (typeof value._original === 'function') {
+        acc[key] = value;
+        return acc;
+      }
+
+      // Patch the function and mark it "patched" by adding a reference to the original function
+      acc[key] = patch(key, value, options);
+      acc[key]._original = value;
+
+      // Deal with functions that also act like an object
+      // TODO might be able to make functions instrumentable so we can omit this extra step
+      if (Object.keys(value).length > 0) {
+        Object.assign(acc[key], instrument({ ...value }, { ...options, path: path.concat(key) }));
+      }
+
       return acc;
     },
     mutate ? obj : construct(obj)
