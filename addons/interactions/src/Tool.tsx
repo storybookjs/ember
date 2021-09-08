e2f7f63e5edac5c11c2b4360c254aac7207c7876
Gert Hengeveld <info@ghengeveld.nl>
2021-09-08 11:29:48 +0200

    Change tool to rerun the story

--- addons/interactions/src/Tool.tsx
+++ addons/interactions/src/Tool.tsx
@@ -1,22 +1,13 @@
-import React, { useCallback } from 'react';
-import { useGlobals } from '@storybook/api';
+import React from 'react';
+import { useChannel } from '@storybook/api';
 import { Icons, IconButton } from '@storybook/components';
-import { TOOL_ID } from './constants';
+import { EVENTS, TOOL_ID } from './constants';
 
 export const Tool = () => {
-  const [{ myAddon }, updateGlobals] = useGlobals();
-
-  const toggleMyTool = useCallback(
-    () =>
-      updateGlobals({
-        myAddon: myAddon ? undefined : true,
-      }),
-    [myAddon]
-  );
-
+  const emit = useChannel({});
   return (
-    <IconButton key={TOOL_ID} title="Play" onClick={toggleMyTool}>
-      <Icons icon="play" />
+    <IconButton key={TOOL_ID} title="Rerun story" onClick={() => emit(EVENTS.RELOAD)}>
+      <Icons icon="sync" />
     </IconButton>
   );
 };
--- addons/interactions/src/preset/manager.ts
+++ addons/interactions/src/preset/manager.ts
@@ -4,9 +4,7 @@ import { ADDON_ID, TOOL_ID, PANEL_ID } from "../constants";
 import { Tool } from "../Tool";
 import { Panel } from "../Panel";
 
-// Register the addon
 addons.register(ADDON_ID, () => {
-  // Register the tool
   addons.add(TOOL_ID, {
     type: types.TOOL,
     title: "Interactions",
@@ -14,7 +12,6 @@ addons.register(ADDON_ID, () => {
     render: Tool,
   });
 
-  // Register the panel
   addons.add(PANEL_ID, {
     type: types.PANEL,
     title: "Interactions",
