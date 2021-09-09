cb2c17d6d236ecda63fea2133dbb7204900d8391
Gert Hengeveld <info@ghengeveld.nl>
2021-09-09 10:10:17 +0200

    Fix linting and TS issues

--- addons/interactions/src/theme.ts
+++ addons/interactions/src/theme.ts
@@ -2,13 +2,13 @@
  * Interaction Testing Theme
  * PLACEHOLDER until SB is updated <3
  */
-interface colors {
+interface Colors {
   pure?: {
     gray?: any;
   };
 }
 
-export const colors: colors = {
+export const colors: Colors = {
   pure: {
     gray: {
       500: '#CCCCCC',
