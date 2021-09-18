8afa74c98ed1e2a7cccad8d31816bc91a5e72c4e
Michael Shilman <michael@lab80.co>
2021-09-18 13:18:09 +0800

    Add ability to pass auto-title to CSF library

--- lib/csf-tools/src/index.ts
+++ lib/csf-tools/src/index.ts
@@ -1,15 +1,15 @@
 import fs from 'fs-extra';
 import mdx from '@mdx-js/mdx';
 
-import { loadCsf } from './CsfFile';
+import { loadCsf, CsfOptions } from './CsfFile';
 import { createCompiler } from './mdx';
 
-export const readCsfOrMdx = async (fileName: string) => {
+export const readCsfOrMdx = async (fileName: string, options: CsfOptions) => {
   let code = (await fs.readFile(fileName, 'utf-8')).toString();
   if (fileName.endsWith('.mdx')) {
     code = await mdx(code, { compilers: [createCompiler({})] });
   }
-  return loadCsf(code);
+  return loadCsf(code, options);
 };
 
 export * from './CsfFile';
