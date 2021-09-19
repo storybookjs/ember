204c26be7c4ed96879d362e28821f4c82e790021
Michael Shilman <michael@lab80.co>
2021-09-19 17:30:03 +0800

    Demo story sorting in examples/react-ts

--- examples/react-ts/preview.js
+++ examples/react-ts/preview.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import { ThemeProvider, convert, themes } from '@storybook/theming';
 
+export const parameters = {
+  options: {
+    // storySort: (a, b) => (
+    //   a[1].title === b[1].title
+    //     ? 0
+    //     : a[1].id.localeCompare(b[1].id, undefined, { numeric: true });
+    // ),
+    storySort: ['Examples', 'Docs', 'Demo'],
+  },
+};
+
 export const decorators = [
   (StoryFn) => (
     <ThemeProvider theme={convert(themes.light)}>
