f3df96e4f953423212946bd8a8d653ae02fc9d75
Tom Coleman <tom@thesnail.org>
2021-09-20 17:13:57 +1000

    Back compatible React types as well as preview 7-0 types

--- app/react/src/client/preview/types-6-0.ts
+++ app/react/src/client/preview/types-6-0.ts
@@ -1,5 +1,5 @@
 import { ComponentType } from 'react';
-import { Args, ComponentAnnotations, StoryAnnotationsOrFn } from '@storybook/csf';
+import { Args, ComponentAnnotations, StoryAnnotations, AnnotatedStoryFn } from '@storybook/csf';
 import { StoryFnReactReturnType } from './types';
 
 export type { Args, ArgTypes, Parameters, StoryContext } from '@storybook/csf';
@@ -17,8 +17,25 @@ export type ReactFramework = {
 export type Meta<TArgs = Args> = ComponentAnnotations<ReactFramework, TArgs>;
 
 /**
- * Story function that represents a component example.
+ * Story function that represents a CSFv2 component example.
  *
  * @see [Named Story exports](https://storybook.js.org/docs/formats/component-story-format/#named-story-exports)
  */
-export type Story<TArgs = Args> = StoryAnnotationsOrFn<ReactFramework, TArgs>;
+export type StoryFn<TArgs = Args> = AnnotatedStoryFn<ReactFramework, TArgs>;
+
+/**
+ * Story function that represents a CSFv3 component example.
+ *
+ * @see [Named Story exports](https://storybook.js.org/docs/formats/component-story-format/#named-story-exports)
+ */
+export type StoryObj<TArgs = Args> = StoryAnnotations<ReactFramework, TArgs>;
+
+/**
+ * Story function that represents a CSFv2 component example.
+ *
+ * @see [Named Story exports](https://storybook.js.org/docs/formats/component-story-format/#named-story-exports)
+ *
+ * NOTE that in Storybook 7.0, this type will be renamed to `StoryFn` and replaced by the current `StoryObj` type.
+ *
+ */
+export type Story<TArgs = Args> = StoryFn<TArgs>;
