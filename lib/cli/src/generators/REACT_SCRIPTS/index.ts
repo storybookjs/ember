c8ad08828cc8526c024fb35e4542f4c02ee81af7
Michael Shilman <michael@lab80.co>
2021-09-30 23:43:46 +0800

    CLI: Limit preset install for old versions of SB

--- lib/cli/src/generators/REACT_SCRIPTS/index.ts
+++ lib/cli/src/generators/REACT_SCRIPTS/index.ts
@@ -27,12 +27,15 @@ const generator: Generator = async (packageManager, npmOptions, options) => {
   )?.version;
   const isCra5 = semver.gte(craVersion, '5.0.0');
   const updatedOptions = isCra5 ? { ...options, builder: CoreBuilder.Webpack5 } : options;
+  // `@storybook/preset-create-react-app` has `@storybook/node-logger` as peerDep
   const extraPackages = ['@storybook/node-logger'];
   if (isCra5) extraPackages.push('webpack');
 
+  // preset v3 is compat with older versions of CRA, otherwise let version float
+  const extraAddons = [`@storybook/preset-create-react-app${isCra5 ? '' : '@3'}`];
+
   await baseGenerator(packageManager, npmOptions, updatedOptions, 'react', {
-    extraAddons: ['@storybook/preset-create-react-app'],
-    // `@storybook/preset-create-react-app` has `@storybook/node-logger` as peerDep
+    extraAddons,
     extraPackages,
     staticDir: fs.existsSync(path.resolve('./public')) ? 'public' : undefined,
     addBabel: false,
