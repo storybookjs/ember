5a9d391e354a614d3bd39c611be60fc812166dd1
Alasdair McLeay <penx@users.noreply.github.com>
2021-10-02 17:39:41 +0100

    Add link to documentation

--- lib/addons/src/types.ts
+++ lib/addons/src/types.ts
@@ -272,6 +272,8 @@ export interface BaseMeta<ComponentType> {
    * Manually set the id of a story, which in particular is useful if you want to rename stories without breaking permalinks.
    *
    * Storybook will prioritize the id over the title for ID generation, if provided, and will prioritize the story.name over the export key for display.
+   *
+   * @see [Sidebar and URLs](https://storybook.js.org/docs/react/configure/sidebar-and-urls)
    */
   id?: string;
 
