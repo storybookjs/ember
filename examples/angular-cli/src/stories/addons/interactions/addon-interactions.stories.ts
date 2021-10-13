54ff20a32c7d2907da92006f57a9cd3578213cff
Yann Braga <yannbf@gmail.com>
2021-10-13 10:48:58 +0200

    add addon-interactions to angular project

---
+++ examples/angular-cli/src/stories/addons/interactions/addon-interactions.stories.ts
@@ -0,0 +1,26 @@
+import { Story, Meta } from '@storybook/angular';
+import { expect } from '@storybook/jest';
+import { within, userEvent } from '@storybook/testing-library';
+
+import { CounterComponent } from './counter/counter.component';
+
+export default {
+  title: 'Addons/Interactions',
+  component: CounterComponent,
+} as Meta;
+
+const Template: Story = (args) => ({
+  props: args,
+});
+
+export const Default: Story = Template.bind({});
+
+Default.play = async ({ canvasElement }) => {
+  // TODO: Testing library does NOT support shadow dom. This will not work.
+  // We can try this instead: https://www.npmjs.com/package/testing-library__dom but it's in beta and hasn't been updated in 8 months
+  const canvas = within(canvasElement);
+  await userEvent.click(await canvas.findByText('Increment'));
+
+  const count = await canvas.findByTestId('count');
+  // await expect(count.textContent).toEqual('1');
+};
