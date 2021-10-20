e1224ee7968dbed52a403b9d5296b345671b4065
Michael Shilman <michael@lab80.co>
2021-10-20 19:06:39 +0800

    CLI: Add automigration for main.js framework

---
+++ lib/cli/src/automigrate/fixes/mainjsFramework.ts
@@ -0,0 +1,66 @@
+import chalk from 'chalk';
+import dedent from 'ts-dedent';
+
+import semver from '@storybook/semver';
+import { ConfigFile, readConfig, writeConfig } from '@storybook/csf-tools';
+
+import { getStorybookInfo } from '../helpers/getStorybookInfo';
+import { Fix } from '../types';
+
+const logger = console;
+
+interface MainjsFrameworkRunOptions {
+  framework: string;
+  main: ConfigFile;
+}
+
+export const mainjsFramework: Fix<MainjsFrameworkRunOptions> = {
+  id: 'mainjsFramework',
+
+  async check({ packageManager }) {
+    const packageJson = packageManager.retrievePackageJson();
+    const { mainConfig, framework, version: storybookVersion } = getStorybookInfo(packageJson);
+    const storybookCoerced = semver.coerce(storybookVersion).version;
+
+    const main = await readConfig(mainConfig);
+    const currentFramework = main.getFieldValue(['framework']);
+    const features = main.getFieldValue(['features']);
+
+    if (currentFramework) return null;
+
+    if (!storybookCoerced) {
+      logger.warn(`Unable to determine storybook version, skipping mainjsFramework fix.`);
+      return null;
+    }
+
+    return features?.breakingChangesV7 ||
+      features?.storyStoreV7 ||
+      semver.gte(storybookCoerced, '7.0.0')
+      ? { main, framework: `@storybook/${framework}` }
+      : null;
+  },
+
+  prompt({ framework }) {
+    const frameworkFormatted = chalk.cyan(`framework: '${framework}'`);
+
+    return dedent`
+      We've detected that your main.js configuration file does not specify the
+      'framework' field, which is a requirement in SB7.0 and above. We can add one
+      for you automatically:
+
+      ${frameworkFormatted}
+
+      More info: ${chalk.yellow(
+        'https://github.com/storybookjs/storybook/blob/next/MIGRATION.md#webpack-5-manager-build'
+      )}
+    `;
+  },
+
+  async run({ result: { main, framework }, dryRun }) {
+    logger.info(`✅ Setting 'framework' to '${framework}' in main.js`);
+    if (!dryRun) {
+      main.setFieldValue(['framework'], framework);
+      await writeConfig(main);
+    }
+  },
+};
