61cc1f60a35b11ae646a04ef0b5efa1fd542ce98
Emily Marigold Klassen <760204+forivall@users.noreply.github.com>
2021-10-28 16:29:58 -0700

    fix(addon-actions): omit sending window object thru the channel

--- addons/actions/src/preview/action.ts
+++ addons/actions/src/preview/action.ts
@@ -4,6 +4,40 @@ import { EVENT_ID } from '../constants';
 import { ActionDisplay, ActionOptions, HandlerFunction } from '../models';
 import { config } from './configureActions';
 
+type SyntheticEvent = any; // import('react').SyntheticEvent;
+const protoWithName = (obj: unknown, name: string): Function | null => {
+  const proto = Object.getPrototypeOf(obj);
+  if (!proto || proto.constructor.name === name) return proto;
+  return protoWithName(proto, name);
+};
+const isReactSyntheticEvent = (e: unknown): e is SyntheticEvent =>
+  Boolean(
+    typeof e === 'object' &&
+      e &&
+      protoWithName(e, 'SyntheticEvent') &&
+      typeof (e as SyntheticEvent).persist === 'function'
+  );
+const serializeArg = <T>(a: T) => {
+  if (isReactSyntheticEvent(a)) {
+    const e: SyntheticEvent = Object.create(
+      a.constructor.prototype,
+      Object.getOwnPropertyDescriptors(a)
+    );
+    e.persist();
+    const viewDescriptor = Object.getOwnPropertyDescriptor(e, 'view');
+    // dont send the entire window object over.
+    const view: unknown = viewDescriptor?.value;
+    if (typeof view === 'object' && view?.constructor.name === 'Window') {
+      Object.defineProperty(e, 'view', {
+        ...viewDescriptor,
+        value: Object.create(view.constructor.prototype),
+      });
+    }
+    return e;
+  }
+  return a;
+};
+
 export function action(name: string, options: ActionOptions = {}): HandlerFunction {
   const actionOptions = {
     ...config,
@@ -14,7 +48,8 @@ export function action(name: string, options: ActionOptions = {}): HandlerFuncti
     const channel = addons.getChannel();
     const id = uuidv4();
     const minDepth = 5; // anything less is really just storybook internals
-    const normalizedArgs = args.length > 1 ? args : args[0];
+    const serializedArgs = args.map(serializeArg);
+    const normalizedArgs = args.length > 1 ? serializedArgs : serializedArgs[0];
 
     const actionDisplayToEmit: ActionDisplay = {
       id,
