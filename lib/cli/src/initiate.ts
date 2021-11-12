f67918077a130b22f963459f07ada132575369b8
Yann Braga <yannbf@gmail.com>
2021-11-12 12:11:46 +0100

    cli: add automigrate to sb init

--- lib/cli/src/initiate.ts
+++ lib/cli/src/initiate.ts
@@ -28,6 +28,7 @@ import raxGenerator from './generators/RAX';
 import serverGenerator from './generators/SERVER';
 import { JsPackageManagerFactory, readPackageJson } from './js-package-manager';
 import { NpmOptions } from './NpmOptions';
+import { automigrate } from './automigrate';
 
 const logger = console;
 
@@ -283,7 +284,7 @@ const projectTypeInquirer = async (options: { yes?: boolean }) => {
   return Promise.resolve();
 };
 
-export function initiate(options: CommandOptions, pkg: Package): Promise<void> {
+export async function initiate(options: CommandOptions, pkg: Package): Promise<void> {
   const welcomeMessage = 'sb init - the simplest way to add a Storybook to your project.';
   logger.log(chalk.inverse(`\n ${welcomeMessage} \n`));
 
@@ -326,8 +327,10 @@ export function initiate(options: CommandOptions, pkg: Package): Promise<void> {
   }
   done();
 
-  return installStorybook(projectType, {
+  await installStorybook(projectType, {
     ...options,
     ...(isEsm ? { commonJs: true } : undefined),
   });
+
+  return automigrate({ yes: true });
 }
