e619a92a96c4b3207c00dd81b92c455c258c4142
Tom Coleman <tom@chromatic.com>
2021-11-19 17:11:40 +1100

    Fix DeepScan issues

--- lib/components/src/blocks/ArgsTable/ArgRow.tsx
+++ lib/components/src/blocks/ArgsTable/ArgRow.tsx
@@ -1,4 +1,4 @@
-import React, { FC, PropsWithChildren } from 'react';
+import React, { FC } from 'react';
 import Markdown from 'markdown-to-jsx';
 import { transparentize } from 'polished';
 import { styled } from '@storybook/theming';
