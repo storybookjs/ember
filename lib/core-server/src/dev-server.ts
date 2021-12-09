2d1c3a35ccc2ea3fcd2509d179f4e059cab9470c
Chris Trevino <chtrevin@microsoft.com>
2021-12-09 11:12:44 -0800

    add headers required to re-enable sharedArrayBuffer

--- lib/core-server/src/dev-server.ts
+++ lib/core-server/src/dev-server.ts
@@ -32,6 +32,10 @@ export async function storybookDevServer(options: Options) {
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    // Required headers to enable SharedArrayBuffer
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/SharedArrayBuffer
+    res.header('Cross-Origin-Opener-Policy', 'same-origin');
+    res.header('Cross-Origin-Embedder-Policy', 'require-corp');
     next();
   });
 
