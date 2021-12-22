bfe22edfbe4a9891e6cd3fd7016b5670b06c392f
Mrau Hu <mrauhu@yandex.ru>
2021-12-23 00:20:19 +0300

    Created default `render()` function for support CSF3 with Vue 3 framework

--- app/vue3/src/client/preview/render.ts
+++ app/vue3/src/client/preview/render.ts
@@ -1,8 +1,21 @@
 import dedent from 'ts-dedent';
 import { createApp, h, shallowRef, ComponentPublicInstance } from 'vue';
 import { RenderContext } from '@storybook/store';
+import { ArgsStoryFn } from '@storybook/csf';
+
 import { StoryFnVueReturnType } from './types';
 import { VueFramework } from './types-6-0';
+
+export const render: ArgsStoryFn<VueFramework> = (props, context) => {
+  const { id, component: Component } = context;
+  if (!Component) {
+    throw new Error(
+      `Unable to render story ${id} as the component annotation is missing from the default export`
+    );
+  }
+
+  return h(Component, props);
+};
 
 export const activeStoryComponent = shallowRef<StoryFnVueReturnType | null>(null);
 
