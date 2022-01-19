bef9e354825e7f60d98524ba840d485dfdcd165f
Yann Braga <yannbf@gmail.com>
2022-01-19 17:18:26 +0100

    test(store): add tests for testing utils

---
+++ lib/store/src/csf/testing-utils/index.test.ts
@@ -0,0 +1,67 @@
+import { composeStory, composeStories } from '.';
+
+describe('composeStory', () => {
+  const meta = {
+    title: 'Button',
+    parameters: {
+      firstAddon: true,
+    },
+    args: {
+      label: 'Hello World',
+      primary: true,
+    },
+  };
+
+  test('should return story with composed args and parameters', () => {
+    const Story = () => {};
+    Story.args = { primary: true };
+    Story.parameters = {
+      parameters: {
+        secondAddon: true,
+      },
+    };
+
+    const composedStory = composeStory(Story, meta);
+    expect(composedStory.args).toEqual({ ...Story.args, ...meta.args });
+    expect(composedStory.parameters).toEqual(
+      // why is this erroring in TS?
+      expect.objectContaining({ ...Story.parameters, ...meta.parameters })
+    );
+  });
+
+  test('should throw an error if Story is undefined', () => {
+    expect(() => {
+      composeStory(undefined, meta);
+    }).toThrow();
+  });
+});
+
+describe('composeStories', () => {
+  test('should call composeStoryFn with stories', () => {
+    const composeConfigFn = jest.fn((v) => v);
+    const module = {
+      default: {
+        title: 'Button',
+      },
+      StoryOne: () => {},
+      StoryTwo: () => {},
+    };
+    const globalConfig = {};
+    composeStories(module, globalConfig, composeConfigFn);
+    expect(composeConfigFn).toHaveBeenCalledWith(module.StoryOne, module.default, globalConfig);
+    expect(composeConfigFn).toHaveBeenCalledWith(module.StoryTwo, module.default, globalConfig);
+  });
+
+  test('should not call composeStoryFn for non-story exports', () => {
+    const composeConfigFn = jest.fn((v) => v);
+    const module = {
+      default: {
+        title: 'Button',
+        excludeStories: /Data/,
+      },
+      mockData: {},
+    };
+    composeStories(module, {}, composeConfigFn);
+    expect(composeConfigFn).not.toHaveBeenCalled();
+  });
+});
