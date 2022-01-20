dca34bc42331e9caabb90772ff57345f50a23786
Norbert de Langen <ndelangen@me.com>
2022-01-20 17:05:14 +0100

    prebundle lib/ui

--- .eslintrc.js
+++ .eslintrc.js
@@ -74,7 +74,7 @@ module.exports = {
     },
     {
       // this package uses pre-bundling, dependencies will be bundled, and will be in devDepenencies
-      files: ['**/lib/theming/**/*', '**/lib/router/**/*'],
+      files: ['**/lib/theming/**/*', '**/lib/router/**/*', '**/lib/ui/**/*'],
       rules: {
         'import/no-extraneous-dependencies': 'off',
       },
