e12ccb14ca1153139b7c49d3d62bf80cedec7353
Yann Braga <yannbf@gmail.com>
2022-01-21 14:57:09 +0100

    add jsdoc to testing utilities

--- app/react/src/client/testing/index.ts
+++ app/react/src/client/testing/index.ts
@@ -24,7 +24,7 @@ let globalStorybookConfig = {
  * Example:
  *```jsx
  * // setup.js (for jest)
- * import { setGlobalConfig } from '@storybook/testing-react';
+ * import { setGlobalConfig } from '@storybook/react';
  * import * as globalStorybookConfig from './.storybook/preview';
  *
  * setGlobalConfig(globalStorybookConfig);
@@ -36,6 +36,32 @@ export function setGlobalConfig(config: GlobalConfig) {
   globalStorybookConfig = composeConfigs([defaultGlobalConfig, config]) as GlobalConfig;
 }
 
+/**
+ * Function that will receive a story along with meta (e.g. a default export from a .stories file)
+ * and optionally a globalConfig e.g. (import * from '../.storybook/preview)
+ * and will return a composed component that has all args/parameters/decorators/etc combined and applied to it.
+ *
+ *
+ * It's very useful for reusing a story in scenarios outside of Storybook like unit testing.
+ *
+ * Example:
+ *```jsx
+ * import { render } from '@testing-library/react';
+ * import { composeStory } from '@storybook/react';
+ * import Meta, { Primary as PrimaryStory } from './Button.stories';
+ *
+ * const Primary = composeStory(PrimaryStory, Meta);
+ *
+ * test('renders primary button with Hello World', () => {
+ *   const { getByText } = render(<Primary>Hello world</Primary>);
+ *   expect(getByText(/Hello world/i)).not.toBeNull();
+ * });
+ *```
+ *
+ * @param story
+ * @param meta - e.g. (import Meta from './Button.stories')
+ * @param [globalConfig] - e.g. (import * as globalConfig from '../.storybook/preview') this can be applied automatically if you use `setGlobalConfig` in your setup files.
+ */
 export function composeStory<GenericArgs>(
   story: TestingStory<GenericArgs>,
   meta: Meta<GenericArgs | any>,
@@ -46,6 +72,31 @@ export function composeStory<GenericArgs>(
   return originalComposeStory<ReactFramework, GenericArgs>(story, meta, projectAnnotations);
 }
 
+/**
+ * Function that will receive a stories import (e.g. `import * as stories from './Button.stories'`)
+ * and optionally a globalConfig (e.g. `import * from '../.storybook/preview`)
+ * and will return an object containing all the stories passed, but now as a composed component that has all args/parameters/decorators/etc combined and applied to it.
+ *
+ *
+ * It's very useful for reusing stories in scenarios outside of Storybook like unit testing.
+ *
+ * Example:
+ *```jsx
+ * import { render } from '@testing-library/react';
+ * import { composeStories } from '@storybook/react';
+ * import * as stories from './Button.stories';
+ *
+ * const { Primary, Secondary } = composeStories(stories);
+ *
+ * test('renders primary button with Hello World', () => {
+ *   const { getByText } = render(<Primary>Hello world</Primary>);
+ *   expect(getByText(/Hello world/i)).not.toBeNull();
+ * });
+ *```
+ *
+ * @param storiesImport - e.g. (import * as stories from './Button.stories')
+ * @param [globalConfig] - e.g. (import * as globalConfig from '../.storybook/preview') this can be applied automatically if you use `setGlobalConfig` in your setup files.
+ */
 export function composeStories<TModule extends StoryFile>(
   storiesImport: TModule,
   globalConfig?: GlobalConfig
