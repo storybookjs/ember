65970379b3dca5d40b5456fc3527727b7f3bf260
Michael Shilman <michael@lab80.co>
2022-03-12 21:00:03 +0800

    Vue: Disable docs preset if not using docs/controls

--- app/vue/src/server/options.ts
+++ app/vue/src/server/options.ts
@@ -4,8 +4,5 @@ import { LoadOptions } from '@storybook/core-common';
 export default {
   packageJson: sync({ cwd: __dirname }).packageJson,
   framework: 'vue',
-  frameworkPresets: [
-    require.resolve('./framework-preset-vue.js'),
-    require.resolve('./framework-preset-vue-docs.js'),
-  ],
+  frameworkPresets: [require.resolve('./preset.js')],
 } as LoadOptions;
