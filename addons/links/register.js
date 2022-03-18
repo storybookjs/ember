2cbae9aa3963f885d42cb811ae320b009b1853e4
Norbert de Langen <ndelangen@me.com>
2022-03-18 13:32:57 +0100

    change addon-links to new structure

--- addons/links/register.js
+++ addons/links/register.js
@@ -1,1 +1,5 @@
-require('./dist/esm/manager');
+require('@storybook/client-logger').once.warn(
+  'register.js is deprecated see https://github.com/storybookjs/storybook/blob/next/MIGRATION.md#deprecated-registerjs'
+);
+
+require('./manager');
