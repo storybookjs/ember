b2ed50b9bb3773db8b1fd614dbcf133aa3f9fa63
Pepijn <>
2022-03-20 12:35:47 +0100

    Change variable naming in tests

--- lib/components/src/controls/Date.test.ts
+++ lib/components/src/controls/Date.test.ts
@@ -2,20 +2,20 @@ import { parseDate, parseTime, formatDate, formatTime } from './Date';
 
 describe('Date control', () => {
   it.each([
-    // caseName, input, expected
+    // name, input, expected
     ['same date', '2022-01-01', '2022-01-01'],
     ['month and day not padded with zeros', '2022-1-1', '2022-01-01'],
     ['different year', '1900-10-1', '1900-10-01'],
-  ])('parse and format date: %s', (a, input, expected) => {
+  ])('parse and format date: %s', (name, input, expected) => {
     expect(formatDate(parseDate(input))).toBe(expected);
   });
 
   it.each([
-    // caseName, input, expected
+    // name, input, expected
     ['same time', '12:00', '12:00'],
     ['hours not padded with zeros', '1:00', '01:00'],
     ['different minutes', '01:30', '01:30'],
-  ])('parse and format time: %s', (a, input, expected) => {
+  ])('parse and format time: %s', (name, input, expected) => {
     expect(formatTime(parseTime(input))).toBe(expected);
   });
 });
