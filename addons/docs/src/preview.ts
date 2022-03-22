b94875522b520f1ec048536911bcfd892d35960a
Michael Shilman <michael@lab80.co>
2022-03-23 03:32:51 +0800

    Addon-docs: manager/preview addon refactor

---
+++ addons/docs/src/preview.ts
@@ -0,0 +1,8 @@
+export const parameters = {
+  docs: {
+    inlineStories: false,
+    getContainer: async () => (await import('./blocks')).DocsContainer,
+    getPage: async () => (await import('./blocks')).DocsPage,
+    iframeHeight: 100,
+  },
+};
