8f8f90382ea75f05dbf4ab566761c9f892c99be4
Norbert de Langen <ndelangen@me.com>
2022-03-29 17:22:24 +0200

    fix thanks to shilman 😘

--- addons/docs/src/preview.ts
+++ addons/docs/src/preview.ts
@@ -1,8 +1,6 @@
 export const parameters = {
   docs: {
-    inlineStories: false,
     getContainer: async () => (await import('./blocks')).DocsContainer,
     getPage: async () => (await import('./blocks')).DocsPage,
-    iframeHeight: 100,
   },
 };
