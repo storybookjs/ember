1259b76bae7aba23d2f042ea3d973920acb663db
Michael Shilman <michael@lab80.co>
2022-04-03 09:11:32 +0800

    Core: Restore preview-web composeConfigs export

--- lib/preview-web/src/index.ts
+++ lib/preview-web/src/index.ts
@@ -1,3 +1,6 @@
+// FIXME: breaks builder-vite, remove this in 7.0
+export { composeConfigs } from '@storybook/store';
+
 export { PreviewWeb } from './PreviewWeb';
 
 export { simulatePageLoad, simulateDOMContentLoaded } from './simulate-pageload';
