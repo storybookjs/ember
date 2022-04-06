cae91d3f9401b43aa058e93b5c9727514d35b2e3
Michael Shilman <michael@lab80.co>
2022-04-06 08:27:13 +0800

    CLI: Upgrade vue3 template to use webpack5 builder

--- lib/cli/src/generators/VUE3/index.ts
+++ lib/cli/src/generators/VUE3/index.ts
@@ -1,7 +1,10 @@
 import { baseGenerator, Generator } from '../baseGenerator';
+import { CoreBuilder } from '../../project_types';
 
 const generator: Generator = async (packageManager, npmOptions, options) => {
-  baseGenerator(packageManager, npmOptions, options, 'vue3', {
+  const updatedOptions = { ...options, builder: CoreBuilder.Webpack5 };
+
+  baseGenerator(packageManager, npmOptions, updatedOptions, 'vue3', {
     extraPackages: ['vue-loader@^16.0.0'],
   });
 };
